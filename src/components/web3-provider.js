29192b1c711eff70d16f7fb1eea86e79a1af5da5
Yu Pan <yupan@originprotocol.com>
2018-05-29 23:01:28 -0700

    Actually changes the username and balance when the user changes

--- src/components/web3-provider.js
+++ src/components/web3-provider.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import { fetchProfile } from 'actions/Profile'
+import { getBalance } from 'actions/Wallet'
 
 import QRCode from 'qrcode.react'
 import Modal from './modal'
@@ -349,17 +351,8 @@ class Web3Provider extends Component {
       this.props.storeWeb3Account(curr)
 
       // force reload on account change
-      if (prev !== null && (!curr) )
-      {
-        window.location.reload()
-      }
-      else
-      {
-        if (prev !== null && confirm("Your account have changed do you wish to reload this page."))
-        {
-           window.location.reload()
-        }
-      }
+      this.props.fetchProfile()
+      this.props.getBalance()
     }
   }
 
@@ -433,6 +426,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
+  fetchProfile: () => dispatch(fetchProfile()),
+  getBalance: () => dispatch(getBalance()),
   storeWeb3Account: addr => dispatch(storeWeb3Account(addr)),
   storeWeb3Intent: intent => dispatch(storeWeb3Intent(intent)),
 })
